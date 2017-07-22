31609791141b1f3bf6ea4fac5e37ebed64f811b7
nandhithakamal <nandhithakml@gmail.com>
2017-07-22 16:55:44 +0530

    Complete with dynamic graphs

--- js/plot.js
+++ js/plot.js
@@ -1,122 +1,91 @@
 var temp = 4;
 var year = 2012;
 var dataSet = [];
-/*var data = [{
-	"year": (year).toString(),
-	"value": temp++
-}, {
-	"year": (year++).toString(),
-	"value": temp++
-}, {
-	"year": (year++).toString(),
-	"value": temp++
-}, {
-	"year": (year++).toString(),
-	"value": temp++
-}, {
-	"year": (year++).toString(),
-	"value": temp++
-}, {
-	"year": (year++).toString(),
-	"value": temp--
-}, {
-	"year": (year++).toString(),
-	"value": temp--
-}, {
-	"year": (year++).toString(),
-	"value": -0.254
-}, {
-	"year": (year++).toString(),
-	"value": 0.47
-}, {
-	"year": (year++).toString(),
-	"value": 0.445
-}, {
-	"year": (year++).toString(),
-	"value": 0.47
-}];
-console.log(data);*/
 
-$.ajax({
-	type: 'POST',
-	contentType: 'application/json',
-	//dataType: "json",
-	url: "http://localhost:8090/fetch",
-	crossDomain: false,
-	success: function(input){
-		dataSet = input;
-		alert(JSON.stringify(input));
-		var chart = AmCharts.makeChart("chartdiv", {
-			"type": "serial",
-			"categoryField": "time",
-			"theme": "light",
-			"marginTop":0,
-			"marginRight": 80,
-			"dataProvider": dataSet,
-			"valueAxes": [{
-				"axisAlpha": 0,
-				"position": "left"
-			}],
-			"graphs": [{
-				"id":"g1",
-				"balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-				"bullet": "round",
-				"bulletSize": 8,
-				"lineColor": "#d1655d",
-				"lineThickness": 2,
-				"negativeLineColor": "#637bb6",
-				"type": "smoothedLine",
-				"valueField": "value"
-			}],
-			"chartScrollbar": {
-				"graph":"g1",
-				"gridAlpha":0,
-				"color":"#888888",
-				"scrollbarHeight":55,
-				"backgroundAlpha":0,
-				"selectedBackgroundAlpha":0.1,
-				"selectedBackgroundColor":"#888888",
-				"graphFillAlpha":0,
-				"autoGridCount":true,
-				"selectedGraphFillAlpha":0,
-				"graphLineAlpha":0.2,
-				"graphLineColor":"#c2c2c2",
-				"selectedGraphLineColor":"#888888",
-				"selectedGraphLineAlpha":1
+loadGraph();
+function loadGraph(){
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		url: "http://localhost:8090/fetch",
+		crossDomain: false,
+		success: function(input){
+			dataSet = input;
+			var chart = AmCharts.makeChart("chartdiv", {
+				"type": "serial",
+				"categoryField": "time",
+				"theme": "light",
+				"marginTop":0,
+				"marginRight": 80,
+				"dataProvider": dataSet,
+				"valueAxes": [{
+					"axisAlpha": 0,
+					"position": "left"
+				}],
+				"graphs": [{
+					"id":"g1",
+					"balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
+					"bullet": "round",
+					"bulletSize": 8,
+					"lineColor": "#d1655d",
+					"lineThickness": 2,
+					"negativeLineColor": "#637bb6",
+					"type": "smoothedLine",
+					"valueField": "value"
+				}],
+				"chartScrollbar": {
+					"graph":"g1",
+					"gridAlpha":0,
+					"color":"#888888",
+					"scrollbarHeight":55,
+					"backgroundAlpha":0,
+					"selectedBackgroundAlpha":0.1,
+					"selectedBackgroundColor":"#888888",
+					"graphFillAlpha":0,
+					"autoGridCount":true,
+					"selectedGraphFillAlpha":0,
+					"graphLineAlpha":0.2,
+					"graphLineColor":"#c2c2c2",
+					"selectedGraphLineColor":"#888888",
+					"selectedGraphLineAlpha":1
 
-			},
-			"chartCursor": {
-				//"categoryBalloonDateFormat": "JJ:NN:SS:QQQ",
-				"cursorAlpha": 0,
-				"valueLineEnabled":true,
-				"valueLineBalloonEnabled":true,
-				"valueLineAlpha":0.5,
-				"fullWidth":true
-			},
-			"dataDateFormat": "JJ:NN:SS.QQQ",
-			"categoryAxis": {
-				"minPeriod": "JJ:NN:SS.QQQ",
-				"parseDates": false,
-				"minorGridAlpha": 0.1,
-				"minorGridEnabled": true
-			},
-			"export": {
-				"enabled": true
-			}
-		});
+				},
+				"chartCursor": {
+					"categoryBalloonDateFormat": "JJ:NN:SS:QQQ",
+					"cursorAlpha": 0,
+					"valueLineEnabled":true,
+					"valueLineBalloonEnabled":true,
+					"valueLineAlpha":0.5,
+					"fullWidth":true
+				},
+				"dataDateFormat": "JJ:NN:SS.QQQ",
+				"categoryAxis": {
+					"minPeriod": "JJ:NN:SS.QQQ",
+					"parseDates": false,
+					"minorGridAlpha": 0.1,
+					"minorGridEnabled": true
+				},
+				"export": {
+					"enabled": true
+				}
+			});
 
-			chart.addListener("rendered", zoomChart);
-			if(chart.zoomChart){
-				chart.zoomChart();
-			}
+				chart.addListener("rendered", zoomChart);
+				if(chart.zoomChart){
+					chart.zoomChart();
+				}
 
-			function zoomChart(){
-				chart.zoomToIndexes(Math.round(chart.dataProvider.length * 0.4), Math.round(chart.dataProvider.length * 0.55));
-			}
+				function zoomChart(){
+					chart.zoomToIndexes(Math.round(chart.dataProvider.length * 0.4), Math.round(chart.dataProvider.length * 0.55));
+				}
 
-	},
-	error: function(error){
-		alert("There is some error!");
-	},
-	processData: false
-});
+		},
+		error: function(error){
+			alert("There is some error!");
+		},
+		processData: false,
+		complete: function() {
+      		setTimeout(loadGraph, 1000);
+    	}
+	});
+}
--- plotter.js
+++ plotter.js
@@ -16,23 +16,18 @@ app.get('/graph', function(req, res){
     res.sendFile("graph.html", {root});
 });
 app.post('/fetch', function(req, res){
-    //var d = fetchFromDb();
-    var sqlQuery = "select * from mq3data limit 10";
+    var sqlQuery = "select * from inputdata order by time desc limit 10";
     con.query(sqlQuery, function (err, result, fields) {
         if (err) throw err;
         var numberOfRows = result.length;
         var row = 0;
-        //console.log(numberOfRows);
         var temp = 1013;
         var yearArr = result[row]["time"];
         console.log(yearArr.toString());
-        //var time = yearArr.toString().split(" ");
-        //var date = time[0];
+
         while(row < numberOfRows){
             var dateTime = result[row]["time"];
             console.log(dateTime);
-            //var arr = dateTime.toString().split(" "));
-            //var dateTime = new Date(currResult);
             var millisecond = dateTime.getMilliseconds();
             var second = dateTime.getSeconds();
             var minute = dateTime.getMinutes();
@@ -40,12 +35,11 @@ app.post('/fetch', function(req, res){
             var year = dateTime.getYear();
             var month = dateTime.getMonth();
             var date = dateTime.getDate();
-            
             var time = hour + ":" + minute + ":" + second + "." + millisecond;
             console.log(time);
             dataFromTable[row] ={
                 "time": time,
-                "value": result[row]["alcoholvalue"]
+                "value": result[row]["distance"]
             };
             row++;
         }
@@ -57,7 +51,7 @@ app.listen(8090, function () {
 });
 
 
-/*var portName = 'COM3';
+var portName = 'COM3';
 var distance = 0;
 var time = 0;
 var connected = false;
@@ -68,7 +62,7 @@ var sp = new Serialport(portName, {
     parity: 'none',
     stopBits: 1,
     flowControl: false
-});*/
+});
 
 var con = mysql.createConnection({
     host: 'localhost',
@@ -76,23 +70,13 @@ var con = mysql.createConnection({
     password: "",
     database: "ultrasonicReads"
 });
-/*
-function getCurrentTime(){
-    var now = new Date();
-    var date = now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate();
-    var time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-    var dateTime = (date+' '+time).toString();
-    return dateTime;
-
-}
 
 function insertIntoDb(){
-    var sql = "INSERT INTO mq3data VALUES ( now(2), " + distance + ");";
+    var sql = "INSERT INTO inputdata VALUES ( now(2), " + distance + ");";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        //console.log("1 record inserted");
     });
-}*/
+}
 
 con.connect(function(err){
     if (err) throw err;
@@ -102,35 +86,29 @@ con.connect(function(err){
     }
 });
 
-
 function fetchFromDb(){
     var sqlQuery = "select * from mq3data limit 10";
     con.query(sqlQuery, function (err, result, fields) {
         if (err) throw err;
         var numberOfRows = result.length;
         var row = 0;
-        //console.log(numberOfRows);
         while(row < numberOfRows){
             dataFromTable[row] = {
                 "time": result[row]["time"],
-                "alcoholValue": result[row]["alcoholvalue"]
+                "distance": result[row]["distance"]
             };
             console.log( dataFromTable[row]);
             row++;
         }
-        //console.log(dataFromTable);
         return dataFromTable;
     });
 }
 
-//fetchFromDb();
-/*
 sp.on('data', function(data) {
-    //time = getCurrentTime();
     distance = parseInt(data.toString('utf8'));
     console.log(data.toString());
     if (connected){
         if(!isNaN(distance))
             insertIntoDb();
     }
-});*/
+});
